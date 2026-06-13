fb2391672e446675c817b01f171acb25489dab54
iMakerKaran <karan.kathait@imaker.technology>
2026-06-13 18:44:04 +0530

    feat: update Google Ads tracking to Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,19 +119,19 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
 
-        {/* Google Ads / Google Tag */}
+        {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18235414628"
+          src="https://www.googletagmanager.com/gtag/js?id=G-85BKEX4SMD"
           strategy="afterInteractive"
         />
 
-        <Script id="google-ads" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'AW-18235414628');
+            gtag('config', 'G-85BKEX4SMD');
           `}
         </Script>
       </body>
